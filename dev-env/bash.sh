d1ea0bda5e584a9adc1a41c5532c654ef144ebfc
dan <dtmoodie@gmail.com>
2017-03-10 15:53:03 -0500

    deploy docker file

--- dev-env/bash.sh
+++ dev-env/bash.sh
@@ -1,7 +1,17 @@
-sudo nvidia-docker run -it --rm \
-  --privileged --env="DISPLAY" \
-  --env="QT_X11_NO_MITSHM=1" \
+xhost +local:root
+
+nvidia-docker run -it --rm \
+  --privileged \
+  --env="DISPLAY" \
+  --user=$USER \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  -v /home/torc/docker/dev-env/build:/build \
+  -v /home/developer/code/docker/dev-env/build:/build \
+  -v /home/developer/code:/home/developer/code \
+  -v /data:/data \
   -p 8888:8888 \
+  -p 39999:39999 \
+  -p 17815:17815 \
+  --net=host \
   dtmoodie/dev-env:release bash
+
+xhost -local:root
